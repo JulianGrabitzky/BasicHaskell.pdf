29800a854905507654f5c9cb109556b73b5891ce
Julian Grabitzky <grabitzkyjulian@gmail.com>
2019-02-26 16:52:54 +0100

    Add PrettyPrinting module

---
+++ src/PrettyPrinting.hs
@@ -0,0 +1,25 @@
+module PrettyPrinting where
+
+import Data.List
+
+class Pretty a where
+  pretty :: a -> String
+
+instance Pretty Term where
+  -- make it fancy
+  pretty term | head (prettyHelper term) == '(' = middle (prettyHelper term)
+              | otherwise                     = prettyHelper term
+    where
+      -- middle :: [String] -> [String]
+      -- remove first and last element of a list
+      middle [] = []
+      middle [x] = []
+      middle xs = tail (init xs)
+      -- prettyHelper :: [Term] -> String
+      -- make a list look pretty
+      prettyHelper (Var name)       = name
+      prettyHelper (Comb name [])   = name
+      prettyHelper (Comb name args) = "(" ++ name ++ " " ++ (prettyList args) ++ ")"
+      -- prettyList :: [Term] -> String
+      -- use pretty on every element on the list
+      prettyList list = intercalate " " (map prettyHelper list)
