692c31c8bf89a34f5f7ec8167e0fefd7989b7d8b
Meerzwiebel <juswerth@gmx.de>
2019-02-26 16:47:06 +0100

    Add Pretty Printing

--- src/Term.hs
+++ src/Term.hs
@@ -1,7 +1,9 @@
 module Term
-  ( VarName, CombName, Term(..)
-  ) where
+( VarName, CombName, Term(..)
+) where
 
+import Data.List
+  
 -- Alias type for variable names.
 -- A variable name is a string.
 type VarName = String
@@ -15,3 +17,25 @@ type CombName = String
 -- or a constructor or function applied to a list of terms.
 data Term = Var VarName | Comb CombName [Term]
   deriving (Eq, Show)
+
+class Pretty a where
+  pretty :: a -> String
+
+instance Pretty Term where
+  -- make it fancy
+  pretty term | head (prettyHelper term) == '(' = middle (prettyHelper term)
+              | otherwise                     = prettyHelper term
+    where
+      -- middle :: [String] -> [String]
+      -- remove first and last element of a list
+      middle [] = []
+      middle [x] = []
+      middle xs = tail (init xs)
+      -- prettyHelper :: [Term] -> String
+      -- make a list look pretty
+      prettyHelper (Var name)       = name
+      prettyHelper (Comb name [])   = name
+      prettyHelper (Comb name args) = "(" ++ name ++ " " ++ (prettyList args) ++ ")"
+      -- prettyList :: [Term] -> String
+      -- use pretty on every element on the list
+      prettyList list = intercalate " " (map prettyHelper list)
