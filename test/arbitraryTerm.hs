40a9042580bd21dfb86a1501152610dcea7d1eb9
Julian Grabitzky <grabitzkyjulian@gmail.com>
2019-02-28 17:54:33 +0100

    Add test terms

--- test/arbitraryTerm.hs
+++ test/arbitraryTerm.hs
@@ -11,3 +11,23 @@ instance Arbitrary Var where
 
 return []
 testAll = $quickCheck
+
+
+t = (Comb "mul" [(Var "x"), (Comb "add" [(Var "y"), (Var "z"), (Var "w")])])
+
+program = (Prog [r1, r2])
+
+r1 = (Rule left1 right1)
+r2 = (Rule left2 right2)
+
+left1 = (Var "x")
+right1 = (Var "1")
+
+left2 = (Var "y")
+right2 = (Comb "add" [(Var "a"), (Var "b")])
+
+t = (Comb "mul" [(Var "x"), (Comb "add" [(Var "y"), (Var "z"), (Var "w")])])
+
+gimmeDemRight :: Maybe (Rhs, Subst) -> Term
+gimmeDemRight Nothing = error "No right term."
+gimmeDemRight (Just (rhs, subst)) = rhs
