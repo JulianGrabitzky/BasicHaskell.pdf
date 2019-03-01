46a0add6a255a5002af28a8d33be96b82c3e79ee
Julian Grabitzky <grabitzkyjulian@gmail.com>
2019-03-01 10:35:19 +0100

    add comment and remove unnecessary functions

--- src/Matching.hs
+++ src/Matching.hs
@@ -12,16 +12,7 @@ match (Comb combName1 list1) (Comb combName2 list2) | combName1    /= combName2 
                                                     | length list1 /= length list2 = Nothing
                                                     | otherwise                    = foldl1 composeMaybeSubst (zipWith match list1 list2)
 
+-- Compose two maybe subst
 composeMaybeSubst :: Maybe Subst -> Maybe Subst -> Maybe Subst
 composeMaybeSubst (Just s1) (Just s2) = Just (compose s1 s2)
 composeMaybeSubst _         _         = Nothing
-
-applyMaybe :: Maybe Subst -> Term -> Term
-applyMaybe (Just s) t = apply s t
-applyMaybe Nothing  _ = error "No match found."
-
-t1 = Comb "add" [(Var "x"), (Var "y")]
-t2 = Comb "add" [(Var "1"), (Var "2")]
-
-s1 = single "x" (Var "1") -- \(Var "x") -> (Var "1")
-s2 = single "y" (Var "2") -- \(Var "y") -> (Var "2")
