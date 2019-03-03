124e3e1309f2d2dcb4a454e0184801d77743a69b
Julian Grabitzky <grabitzkyjulian@gmail.com>
2019-03-03 18:03:40 +0100

    \(. .)/

--- src/Evaluation.hs
+++ src/Evaluation.hs
@@ -12,8 +12,10 @@ import Prog
 type Strategy = Prog -> Term -> [Pos]
 
 loStrategy :: Strategy
+loStrategy = createStrategy leftOf above
 
 liStrategy :: Strategy
+liStrategy = createStrategy leftOf below
 
 roStrategy :: Strategy
 
@@ -22,6 +24,16 @@ riStrategy :: Strategy
 poStrategy :: Strategy
 
 piStrategy :: Strategy
+
+-- Vllt wird das ja was
+createStrategy :: (Pos -> Pos -> Bool) ->
+                  (Pos -> Pos -> Bool) ->
+                  Strategy
+createStrategy func1 func2 prog term
+    | isNormalForm prog term = []
+    | otherwise =
+        -- vergleiche Positionen irgendwie mit functionen oder so
+        foldl1 (\pos1 pos2 -> ) $ reduciblePos prog term
 
 reduceWith :: Strategy -> Prog -> Term -> Maybe Term
 
