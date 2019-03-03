55ca88ef00cae92a51cc1c073208a06e9851bd89
Julian Grabitzky <grabitzkyjulian@gmail.com>
2019-03-03 22:30:36 +0100

    Add new prop for QuickCheck

--- test/testPrettyPrinting.hs
+++ test/testPrettyPrinting.hs
@@ -1,9 +1,17 @@
 {-# LANGUAGE TemplateHaskell #-}
+module TestPrettyPrinting where
 
 import PrettyPrinting
+import Term
 import Test.QuickCheck
 
-prop_PrettyPrinting :: Term -> Bool
+prop_Var :: String -> Bool
+prop_Var s = pretty (Var s) == s
+
+prop_Comb :: String -> String -> Bool
+prop_Comb s1 s2 = pretty (Comb s1 [Comb s2 []]) == s1 ++ " " ++ s2
 
 return []
-testAll = $quickCheck
+
+testPretty :: IO Bool
+testPretty = $quickCheckAll
