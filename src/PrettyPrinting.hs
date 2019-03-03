6023f492da5bd68ad440530da42eb6efe0744a39
Julian Grabitzky <grabitzkyjulian@gmail.com>
2019-03-03 21:06:21 +0100

    style cleanup and new implmentation of pretty

--- src/PrettyPrinting.hs
+++ src/PrettyPrinting.hs
@@ -1,25 +1,23 @@
-module PrettyPrinting where
+module PrettyPrinting
+    ( Pretty, pretty
+    ) where
 
 import Term
 
 class Pretty a where
   pretty :: a -> String
 
+-- sleek look for term
 instance Pretty Term where
-  -- make it fancy
-  pretty term | head (prettyHelper term) == '(' = middle (prettyHelper term)
-              | otherwise                       = prettyHelper term
-    where
-      -- middle :: [String] -> [String]
-      -- remove first and last element of a list
-      middle [] = []
-      middle [_] = []
-      middle xs = tail (init xs)
-      -- prettyHelper :: [Term] -> String
-      -- make a list look pretty
-      prettyHelper (Var name)       = name
-      prettyHelper (Comb name [])   = name
-      prettyHelper (Comb name args) = "(" ++ name ++ " " ++ (prettyList args) ++ ")"
-      -- prettyList :: [Term] -> String
-      -- use pretty on every element on the list
-      prettyList list = unwords (map prettyHelper list)
+    pretty (Var name) = name
+    pretty (Comb name []) = name
+    pretty (Comb name list) = name ++ " " ++ (prettyList list)
+        where
+            -- prettyHelper :: [Term] -> String
+            prettyHelper (Var varName)       = varName
+            prettyHelper (Comb varName [])   = varName
+            prettyHelper (Comb combName args) = "(" ++ combName ++ " " ++ (prettyList args) ++ ")"
+            -- prettyList :: [Term] -> String
+            -- use pretty on every element on the list
+            -- and add whitespaces between the elements
+            prettyList args = unwords (map prettyHelper args)
