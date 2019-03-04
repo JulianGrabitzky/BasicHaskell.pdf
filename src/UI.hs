87bd1ed53010b9fbc5f06b652cf4949ae56b38cd
Julian Grabitzky <grabitzkyjulian@gmail.com>
2019-03-04 19:31:00 +0100

    add sendHelp and firstContact

--- src/UI.hs
+++ src/UI.hs
@@ -2,24 +2,40 @@ module UI where
 
 import Prog
 import Evaluation
+import PrettyPrinting
 
-data Status = Status Maybe(String, Prog), Strategy
+type State = (FilePath, Prog, Strategy)
 
 main :: IO()
 main = do
-        putStr "Welcome to Simple Haskell!"
-        putStr "Type :help for help."
-        firstContact (Status Nothing loStrategy)
+        putStrLn "Welcome to Simple Haskell!"
+        putStrLn "Type :help for help."
+        firstContact ( "> "  ,Prog [] ,loStrategy)
 
-firstContact :: Status -> IO()
-firstContact (Status program _ ) = do
-    case program of
-        Nothing -> putChar '>'
+firstContact :: State -> IO()
+firstContact state@(filePath, program, strategy) = do
+    putStr filePath
+    userIn <- getLine
+    case userIn of
+      | userIn == ":l" || userIn == ":load" -> --unload und was ist wenn der User leerzeichen dahinter einfügt
+      | take 3 userIn == ":l " || take 6 userIn == ":load " -> -- load
+      | take 2 userIn == ":r" || take 7 userIn == ":reload" -> --reload
+      | take 2 userIn == ":q" || take 5 userIn == ":quit" -> -- bye bye
+      | take 2 userIn == ":h" || take 5 userIn == ":help" -> sendHelp state
+      | take 3 userIn == ":s " || take 5 userIn == ":set " -> -- set strat
+      | otherwise -> -- Es fehlt noch die Termevaluation und ungültige Eingabe?
 
-helpMe :: Nothing
 
-loadFile :: Nothing
-
-unloadFile :: Nothing
-
-setStrategy :: Nothing
+sendHelp :: State -> IO()
+sendHelp state = do
+    putStrLn "Commands available from the prompt:"
+    putStrLn "  <expression>       Evaluates the specified expression."
+    putStrLn "  :h[elp]            Shows this help message."
+    putStrLn "  :l[oad] <file>     Loads the specified file."
+    putStrLn "  :l[oad]            Unloads the currently loaded file."
+    putStrLn "  :r[eload]          Reloads the lastly loaded file."
+    putStrLn "  :s[et] <strategy>  Sets the specified evaluation strategy"
+    putStrLn "                     where <strategy> is one of 'lo', 'li',"
+    putStrLn "                     'ro', 'ri', 'po', or 'pi'."
+    putStrLn "  :q[uit]            Exits the interactive environment."
+    firstContact state
