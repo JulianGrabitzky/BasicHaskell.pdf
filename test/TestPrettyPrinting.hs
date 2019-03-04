50797370e168217314de98cd5d041362bc51b5a6
Julian Grabitzky <grabitzkyjulian@gmail.com>
2019-03-04 10:22:45 +0100

    add new prop

--- test/TestPrettyPrinting.hs
+++ test/TestPrettyPrinting.hs
@@ -11,6 +11,10 @@ prop_Var s = pretty (Var s) == s
 prop_Comb :: String -> String -> Bool
 prop_Comb s1 s2 = pretty (Comb s1 [Comb s2 []]) == s1 ++ " " ++ s2
 
+prop_CombWithList :: Bool
+prop_CombWithList = pretty (Comb "mul" [(Var "x"), (Comb "add" [(Var "y"), (Var "z"), (Var "w")])])
+                    == "mul x (add y z w)"
+
 return []
 
 testPretty :: IO Bool
