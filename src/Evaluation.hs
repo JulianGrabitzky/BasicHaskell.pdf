5db57d147a581721d418e4233cd323e1f59d5c36
Julian Grabitzky <grabitzkyjulian@gmail.com>
2019-03-04 13:07:37 +0100

    add all Strategy

--- src/Evaluation.hs
+++ src/Evaluation.hs
@@ -1,5 +1,6 @@
 module Evaluation
-    (Strategy, loStrategy, liStrategy)--, roStrategy, riStrategy, poStrategy,piStrategy)
+    (Strategy, loStrategy, liStrategy, roStrategy, riStrategy,
+     poStrategy, piStrategy)
     where
 
 import Reduction
@@ -22,12 +23,25 @@ roStrategy = createStrategy above rightOf
 riStrategy :: Strategy
 riStrategy = createStrategy below rightOf
 
--- poStrategy :: Strategy
+poStrategy :: Strategy
+poStrategy = createParStrategy above
 
--- piStrategy :: Strategy
+piStrategy :: Strategy
+piStrategy = createParStrategy below
 
-createStrategy :: (Pos -> Pos -> Bool) ->
-                  (Pos -> Pos -> Bool) ->
+-- look for the max element in comparison to every other element in the same subtree
+createParStrategy :: (Pos -> Pos -> Bool) -> -- above/below testing in tree
+                     Strategy
+createParStrategy ab prog term = filter (ownFilter $ reduciblePos prog term) (reduciblePos prog term)
+  where
+    ownFilter :: [Pos] -> Pos -> Bool
+    ownFilter [] _ = True
+    ownFilter (p:ps) pos | ab p pos = False
+                         | otherwise = ownFilter ps pos
+
+-- create non-parallel evaluation strategy
+createStrategy :: (Pos -> Pos -> Bool) -> -- above/below testing in tree
+                  (Pos -> Pos -> Bool) -> --  left/right testing in tree
                   Strategy
 createStrategy ab lr prog term | isNormalForm prog term = []
                                | otherwise =
@@ -38,31 +52,6 @@ createStrategy ab lr prog term | isNormalForm prog term = []
       helper p1 (p2:ps) | (ab p1 p2) || (lr p1 p2) = helper p1 ps
                         | otherwise                = helper p2 ps
 
-
-program = (Prog [r1, r2])
-r1 = (Rule left1 right1)
-r2 = (Rule left2 right2)
-
-left1 = (Var "x")
-right1 = (Var "y")
-
-left2 = (Var "y")
-right2 = (Var "z")
-
-t1 = Comb "add" [(Var "x"), (Var "y")]
-t2 = Comb "add" [(Var "1"), (Var "2")]
 -- reduceWith :: Strategy -> Prog -> Term -> Maybe Term
 
 -- evaluateWith :: Strategy -> Prog -> Term -> Term
-
-{-
--- Vllt wird das ja was
-createStrategy :: (Pos -> Pos -> Bool) -> (Pos -> Pos -> Bool) -> Strategy
-createStrategy func1 func2 prog term
-| isNormalForm prog term = []
-| otherwise =
-  -- vergleiche Positionen irgendwie mit functionen oder so
-  foldl1 (\pos1 pos2 -> if func1 pos1 pos2 || func2 pos1 pos2
-    then p1
-    else p2) $ reduciblePos prog term
--}
