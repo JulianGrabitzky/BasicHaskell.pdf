7447eac83dbd12317717abff0d1046160d8e5922
Meerzwiebel <juswerth@gmx.de>
2019-03-05 12:10:04 +0100

    fix ui

--- src/UI.hs
+++ src/UI.hs
@@ -3,6 +3,8 @@ module UI where
 import Prog
 import Evaluation
 import PrettyPrinting
+import Data.List
+import Parser
 
 type State = (FilePath, Prog, Strategy)
 
@@ -10,25 +12,68 @@ main :: IO()
 main = do
         putStrLn "Welcome to Simple Haskell!"
         putStrLn "Type :help for help."
-        firstContact ( "> "  ,Prog [] ,loStrategy)
+        firstContact ( "> ", Prog [], loStrategy)
 
 firstContact :: State -> IO()
-firstContact state@(filePath, program, strategy) = do
+firstContact state@(filePath, _, _) = do
     putStr filePath
     userIn <- getLine
     case userIn of
-      | userIn == ":l" || userIn == ":load" -> --unload und was ist wenn der User leerzeichen dahinter einfügt
-      | take 3 userIn == ":l " || take 6 userIn == ":load " -> -- load
-      | take 2 userIn == ":r" || take 7 userIn == ":reload" -> --reload
-      | take 2 userIn == ":q" || take 5 userIn == ":quit" -> -- bye bye
-      | take 2 userIn == ":h" || take 5 userIn == ":help" -> sendHelp state
-      | take 3 userIn == ":s " || take 5 userIn == ":set " -> -- set strat
-      | otherwise -> do
-        evaluationResult <- -- Es fehlt noch die Termevaluation und darin das Errorhandeling
-        putStr evaluationResult
-        firstContact state
+        u |        u == ":l"  ||        u == ":load"   -> unloadProgram state
+          | take 3 u == ":l " || take 6 u == ":load "  -> loadProgram state (last $ words u)
+          | take 2 u == ":r"  || take 7 u == ":reload" -> reloadProgram state
+          | take 2 u == ":q"  || take 5 u == ":quit"   -> putStr "Bye."
+          | take 2 u == ":h"  || take 5 u == ":help"   -> sendHelp state
+          | take 3 u == ":s " || take 5 u == ":set "   -> setStrategy state (last $ words u)
+          | otherwise -> evaluate state u
 
+-- evaluate a user input
+evaluate :: State -> String -> IO()
+evaluate state@(_, program, strategy) input = do
+        case parse input of
+            (Left  errorMsg) -> putStr errorMsg >> firstContact state
+            (Right term)     -> (putStr $ pretty $ evaluateWith strategy program term) >> firstContact state
 
+-- get the name of a program from a file path
+getName :: String -> String
+getName input = take (length fileName - 3) fileName
+    where fileName = drop 1 (last (splitOn input '/'))
+
+-- split a string on a given character
+splitOn :: String -> Char -> [String]
+splitOn str char = groupBy (\_ b -> b /= char) str
+
+-- unload a program
+unloadProgram :: State -> IO()
+unloadProgram (filePath, _, strategy) = do
+        putStr ("Unloading " ++ getName filePath ++ ".")
+        firstContact ("> ", Prog [], strategy)
+
+-- load a program from a user input
+loadProgram :: State -> String -> IO()
+loadProgram state@(_, _, strategy) path = do
+        parsedFile <- parseFile path
+        case parsedFile of
+                (Left errorMsg)    -> putStr errorMsg >> firstContact state
+                (Right newProgram) -> putStr "loaded." >> firstContact (path, newProgram, strategy)
+
+-- reload a program
+reloadProgram :: State -> IO()
+reloadProgram state@(filePath, _, _) = putStr "Reloading program" >> loadProgram state filePath
+
+-- set the evaluation strategy
+setStrategy :: State -> String -> IO()
+setStrategy (filePath, program, strategy) newStrat =
+    case newStrat of
+        "lo" -> putStr "Set evaluation strategy to leftmost outermost."  >> firstContact (filePath, program, loStrategy)
+        "li" -> putStr "Set evaluation strategy to leftmost innermost."  >> firstContact (filePath, program, liStrategy)
+        "ro" -> putStr "Set evaluation strategy to rightmost outermost." >> firstContact (filePath, program, roStrategy)
+        "ri" -> putStr "Set evaluation strategy to rightmost innermost." >> firstContact (filePath, program, riStrategy)
+        "po" -> putStr "Set evaluation strategy to parallel outermost."  >> firstContact (filePath, program, poStrategy)
+        "pi" -> putStr "Set evaluation strategy to parallel innermost."  >> firstContact (filePath, program, piStrategy)
+        _    -> putStr "Invalid evaluation strategy."                    >> firstContact (filePath, program, strategy)
+
+-- print helping screen
 sendHelp :: State -> IO()
 sendHelp state = do
     putStrLn "Commands available from the prompt:"
@@ -42,3 +87,4 @@ sendHelp state = do
     putStrLn "                     'ro', 'ri', 'po', or 'pi'."
     putStrLn "  :q[uit]            Exits the interactive environment."
     firstContact state
+
