65b5d75e4722dc21975b65edb534e78a028a8bba
Julian Grabitzky <grabitzkyjulian@gmail.com>
2019-03-08 16:37:04 +0100

    fix linter hints

--- test/ArbitraryTerm.hs
+++ test/ArbitraryTerm.hs
@@ -11,23 +11,23 @@ return []
 testAll = $quickCheck
 
 
-t = (Comb "mul" [(Var "x"), (Comb "add" [(Var "y"), (Var "z"), (Var "w")])])
+t = Comb "mul" [Var "x", (Comb "add" [Var "y", Var "z", Var "w"])]
 
-program = (Prog [r1, r2])
+program = Prog [r1, r2]
 
-r1 = (Rule left1 right1)
-r2 = (Rule left2 right2)
+r1 = Rule left1 right1
+r2 = Rule left2 right2
 
-left1 = (Var "x")
-right1 = (Var "1")
+left1 = Var "x"
+right1 = Var "1"
 
-left2 = (Var "y")
-right2 = (Comb "add" [(Var "a"), (Var "b")])
+left2 = Var "y"
+right2 = Comb "add" [Var "a", Var "b"]
 
-t = (Comb "mul" [(Var "x"), (Comb "add" [(Var "y"), (Var "z"), (Var "w")])])
+t = Comb "mul" [Var "x", Comb "add" [Var "y", Var "z", Var "w"]]
 
-t1 = Comb "add" [(Var "x"), (Var "y")]
-t2 = Comb "add" [(Var "1"), (Var "2")]
+t1 = Comb "add" [Var "x", Var "y"]
+t2 = Comb "add" [Var "1", Var "2"]
 
 s1 = single "x" (Var "1") -- \(Var "x") -> (Var "1")
 s2 = single "y" (Var "2") -- \(Var "y") -> (Var "2")
